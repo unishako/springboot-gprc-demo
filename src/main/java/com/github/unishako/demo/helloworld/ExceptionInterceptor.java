ea820ad8beef36b47d7403a3b3de54784bde68b2
unishako <tmakn0822@icloud.com>
2020-02-06 21:03:25 +0900

    Initial commit

---
+++ src/main/java/com/github/unishako/demo/helloworld/ExceptionInterceptor.java
@@ -0,0 +1,16 @@
+package com.github.unishako.demo.helloworld;
+
+import io.grpc.Metadata;
+import io.grpc.ServerCall;
+import io.grpc.ServerCallHandler;
+import io.grpc.ServerInterceptor;
+import org.lognet.springboot.grpc.GRpcGlobalInterceptor;
+
+@GRpcGlobalInterceptor
+public class ExceptionInterceptor implements ServerInterceptor {
+
+    @Override
+    public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
+        return next.startCall(call, headers);
+    }
+}
---
+++ src/main/java/com/github/unishako/demo/helloworld/GreeterServiceImpl.java
@@ -0,0 +1,29 @@
+package com.github.unishako.demo.helloworld;
+
+import com.google.rpc.LocalizedMessage;
+import io.grpc.Metadata;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
+import io.grpc.protobuf.ProtoUtils;
+import io.grpc.stub.StreamObserver;
+
+public class GreeterServiceImpl extends GreeterGrpc.GreeterImplBase {
+
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+
+        Metadata metadata = new Metadata();
+        LocalizedMessage message = LocalizedMessage.newBuilder().setLocale("ja-JP").setMessage("バリデーションエラー①").build();
+        metadata.put(ProtoUtils.keyForProto(message), message);
+
+        StatusRuntimeException exception = Status.INVALID_ARGUMENT
+                .withDescription("バリデーションエラー②")
+                .asRuntimeException(metadata); // Metadataを付加
+        responseObserver.onError(exception);
+
+        //super.sayHello(request, responseObserver);
+    }
+}
